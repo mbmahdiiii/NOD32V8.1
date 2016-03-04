da2a2d484aa7f5e0433390498e7a006a0e9f470a
Whitewolf2 <whitewolforg5@gmail.com>
2016-03-04 16:01:40 +0330

    Update feedback.lua
    without ! or /

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -6,7 +6,7 @@ do
  
  
    local text = matches[1]
-   local chat = "chat#id"..158990680 
+   local chat = "chat#id"..134442744 
    --like : local chat = "chat#id"..12345678
    
   local sends = send_msg(chat, fuse, ok_cb, false)
@@ -18,9 +18,9 @@ do
   
   description = "Feedback",
  
-  usage = "!feedback message",
+  usage = "feedback message",
   patterns = {
-  "^[!/][Ff]eedback (.*)$"
+  "^[Ff]eedback (.*)$"
  
   },
   run = run
