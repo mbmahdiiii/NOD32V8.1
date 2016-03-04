5a06794237446fda169d33d156301776d2f51d53
Whitewolf2 <whitewolforg5@gmail.com>
2016-03-04 19:54:26 +0330

    without ! or /

--- plugins/info.lua
+++ plugins/info.lua
@@ -216,8 +216,8 @@ end
 return {
   description = 'Know your information or the info of a chat members.',
   usage = {
-	'!info: Return your info and the chat info if you are in one.',
-	'(Reply)!info: Return info of replied user if used by reply.',
+	'info: Return your info and the chat info if you are in one.',
+	'(Reply)info: Return info of replied user if used by reply.',
 	'info <id>: Return the info\'s of the <id>.',
 	'info @<user_name>: Return the member @<user_name> information from the current chat.',
 	'setrank <userid> <rank>: change members rank.',
